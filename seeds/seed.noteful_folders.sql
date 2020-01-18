e0df256d37371273bc2917aac15fa2f4ab5aec30
zoeferencova <zoeferencova@gmail.com>
2020-01-18 17:13:47 -0500

    added notes service and folders service

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,5 @@
+INSERT INTO noteful_folders (id, name) 
+    VALUES
+        (1, 'Test folder 1'),
+        (2, 'Test folder 2'),
+        (3, 'Test folder 3');
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,7 @@
+INSERT INTO noteful_notes (id, name, modified, folder_id, content) 
+    VALUES
+        (1, 'Test note 1', '2019-01-03T00:00:00.000Z', 1, 'Test content'),
+        (2, 'Test note 2', '2019-01-03T00:00:00.000Z', 2, 'Test content'),
+        (3, 'Test note 3', '2019-01-03T00:00:00.000Z', 3, 'Test content'),
+        (4, 'Test note 4', '2019-01-03T00:00:00.000Z', 2, 'Test content'),
+        (5, 'Test note 5', '2019-01-03T00:00:00.000Z', 1, 'Test content');
